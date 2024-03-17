e8f9f803b6596fb390d747a870dca8b49ac45dd1
Clive Ouma <132348880+CliveOuma@users.noreply.github.com>
2024-03-18 00:22:40 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server is Running"));
+
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "cliveouma5@gmail.com",
+    pass: "akej zvle iqvj spwi"
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send Email");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: "cliveouma5@gmail.com",
+    subject: "Email from Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
---
+++ frontend/src/app/components/Project.jsx
@@ -0,0 +1,62 @@
+"use client"
+import React,{useState} from 'react'
+import ProjectCard from './ProjectCard'
+import ProjectTag from './ProjectTag'
+
+const projectsData = [
+    {
+      id: 1,
+      title: "Ecommerce Website",
+      description: "An Ecommerce Website that allows customers to add and remove products from their cart and authentication of users",
+      image: "/images/projects/shop-image.png",
+      tag: ["All", "Web"],
+      gitUrl: "https://github.com/CliveOuma/my-ecommerce",
+      previewUrl: "https://my-ecommerce-client.vercel.app",
+    },
+    {
+      id: 2,
+      title: "Student Management System",
+      description: "A comprehensive solution for managing students and students to access their information securely.",
+      image: "/images/projects/std-image.png",
+      tag: ["All"],
+      gitUrl: "https://github.com/CliveOuma/student-management-system",
+      previewUrl: "/",
+    },
+]  
+
+const Project = () => {
+    const [tag, setTag] = useState("All");
+
+    const handleTagChange = (newTag) => {
+        setTag(newTag);
+    };
+    const filteredProjects = projectsData.filter((project) => 
+        project.tag.includes(tag)
+    );
+  return (
+    <>
+    <h2 className='text-center text-4xl font-bold mt-4  pb-5 text-white'>My Works</h2>
+    <section className="project my-12 md:my-12 pt-4  gap-2" id="project">
+    <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
+    <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === "All"}/>
+    <ProjectTag onClick={handleTagChange} name="Web" isSelected={tag === "Web"}/>
+    </div>
+    <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
+        {
+            filteredProjects.map((project) => (
+                <ProjectCard key={project.id}
+                title={project.title}
+                description= {project.description}
+                imgUrl={project.image}
+                gitUrl={project.gitUrl}
+                previewUrl={project.previewUrl}
+                />
+            ))
+        }
+    </div>
+    </section>
+    </>
+  )
+}
+
+export default Project
